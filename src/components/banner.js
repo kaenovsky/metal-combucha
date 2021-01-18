2b886d66b410aa1fecc9834137f3b5f06b84d0bf
kaen <kaen@alpogo.com>
2021-01-18 20:38:48 -0300

    - update banner flema

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,7 +3,9 @@ import React from "react"
 const Banner = () => (
     <div>
         <div className='banner-web'>
-              
+           <a href="https://alpogo.com/evento/flema-en-taberna-odin-live-3681" rel="noreferrer" alt="banner-flema" target="_blank">
+               <img src="https://taberna-odin-bucket.eu-central-1.linodeobjects.com/flema-flyer-20-02.jpg"></img>
+            </a> 
         </div>
     </div>
 )
