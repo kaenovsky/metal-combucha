bbc92204650745fc76bc4ef32ea7bc049e337d70
kaen <kaen@alpogo.com>
2021-02-01 14:20:45 -0300

    ep 22

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/4Q7qLSJbgDvPQaj3gYU1Bs"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/7rb8H2fPTTj6KoqOihovEU"
                 title="episode"
                 height="232" 
