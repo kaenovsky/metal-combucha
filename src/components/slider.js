a91581cbbdd26345c74ccf4555b1e09026769364
kaen <kaen@alpogo.com>
2021-04-25 18:27:56 -0300

    ep 30

--- src/components/slider.js
+++ src/components/slider.js
@@ -13,6 +13,15 @@ export default function SimpleSlider() {
     };
     return (
       <Slider {...settings} className='slider-wrapper'>
+          <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/1VbSom5tV0DPwQvX6t6oS6"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>
         <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/5dAR0cKiYqr0030eqBMX4c"
                 title="episode"
@@ -60,24 +69,6 @@ export default function SimpleSlider() {
         </div>
         <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/3eUbWE2adRa7NLg56TbAK4"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/6Vz573muEpsQdRLJcDfith"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/4Q7qLSJbgDvPQaj3gYU1Bs"
                 title="episode"
                 height="232" 
                 frameborder="0" 
