7c1897908e973bc73cb66507e053b9c32b2da957
kaen <kaen@alpogo.com>
2021-05-27 18:40:26 -0300

    - ep 34

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/5eYUigLJ4zhJCkIQpi8fOa"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/4dl5a1hIr25wSKpaTIV2i1"
                 title="episode"
                 height="232" 
@@ -60,15 +69,6 @@ export default function SimpleSlider() {
         </div>
         <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/0mKLC3id4PvryMSiyOoj6G"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/0NNL3gGzUEko7S8gL192IM"
                 title="episode"
                 height="232" 
                 frameborder="0" 
