2c75211881af0520bcf9f29ed09a254e2d501e76
kaen <kaen@alpogo.com>
2021-07-30 01:28:23 -0300

    - link youtube

--- src/components/social.js
+++ src/components/social.js
@@ -8,7 +8,7 @@ const Social = () => (
         <div className='link-social-networks'>
               <a rel="noreferrer" href='https://www.facebook.com/tabernaodinlive' target='_blank'><img alt='facebook' src='https://taberna-odin-bucket.eu-central-1.linodeobjects.com/facebook.svg'></img></a>
               <a rel="noreferrer" href='https://www.instagram.com/tabernaodinlive' target='_blank'><img alt='instagram' src='https://taberna-odin-bucket.eu-central-1.linodeobjects.com/instagram.svg'></img></a>
-              <a rel="noreferrer" href='https://www.youtube.com/playlist?list=PL-Zj37MYV3-xq_yFmb21Bbb2TrZCpJLDb' target='_blank'><img alt='youtube' src='https://taberna-odin-bucket.eu-central-1.linodeobjects.com/youtube.svg'></img></a>
+              <a rel="noreferrer" href='https://www.youtube.com/channel/UCOnMd5rB83-fZBZFVr_7UmA' target='_blank'><img alt='youtube' src='https://taberna-odin-bucket.eu-central-1.linodeobjects.com/youtube.svg'></img></a>
               <a rel="noreferrer" href='https://open.spotify.com/show/7zsXG6svLmS4sUBJrSBqRx?si=vYNqFX-pSFKfBcCO0FVaHQ' target='_blank'><img alt='spotify' src='https://taberna-odin-bucket.eu-central-1.linodeobjects.com/spotify.svg'></img></a>
         </div>
     </div>
