354d30f6b5d9cc6b134ce157dbfb5892f4972964
kaenovsky <martinkaen@gmail.com>
2021-08-19 21:01:16 -0300

    - ep 45

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/1VHvzfglkgnN73bLvNJR2A"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/07aSz2t4ndjsjvJAUMAeNC"
                 title="episode"
                 height="232" 
@@ -57,16 +66,7 @@ export default function SimpleSlider() {
                 allowtransparency="true" 
                 allow="encrypted-media">
             </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/27gxSZYbkVXVEJazJGHFE9"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
+        </div>        
       </Slider>
     );
   }
