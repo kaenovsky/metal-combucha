fae79c14b41d772aeb5491d5bc8c4c645ebc222c
kaenovsky <martinkaen@gmail.com>
2021-08-29 16:54:39 -0300

    -ep 46

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/3rCvW6sVez79iwnT6MlAgk"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/1VHvzfglkgnN73bLvNJR2A"
                 title="episode"
                 height="232" 
@@ -57,16 +66,7 @@ export default function SimpleSlider() {
                 allowtransparency="true" 
                 allow="encrypted-media">
             </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/2dDT6CU8ZFiFq3nEKzweDe"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
+        </div>        
       </Slider>
     );
   }
