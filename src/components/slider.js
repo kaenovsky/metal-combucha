4b69578d86b9f2e74498c536f6b363c0541a88b2
kaenovsky <martinkaen@gmail.com>
2021-10-21 15:35:57 -0300

    ep 51

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/3OuAVieDgYazljtghiMgm9"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>  
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/4GzLtj0GkXsC2GdQBfkvIB"
                 title="episode"
                 height="232" 
@@ -57,16 +66,7 @@ export default function SimpleSlider() {
                 allowtransparency="true" 
                 allow="encrypted-media">
             </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/1VHvzfglkgnN73bLvNJR2A"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>                          
+        </div>                        
       </Slider>
     );
   }
