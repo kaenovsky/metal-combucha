4baa2a69f9d89db60d5d02f7409482e7996aa9ba
kaenovsky <martinkaen@gmail.com>
2021-11-09 12:55:18 -0300

    - ep 54

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/5MW5PQapoz526nGdH59qfR"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div>
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/5JcXoCmNDbBm0HS3TLScZg"
                 title="episode"
                 height="232" 
@@ -57,16 +66,7 @@ export default function SimpleSlider() {
                 allowtransparency="true" 
                 allow="encrypted-media">
             </iframe>
-        </div> 
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/2tfKiFA2yXoFGcfnRX38HM"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>                                      
+        </div>                                     
       </Slider>
     );
   }
