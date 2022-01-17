5c3a304820c7c6f11a451b04ee0771c4bcaee219
kaenovsky <martinkaen@gmail.com>
2022-01-17 11:25:52 -0300

    - ep 62

--- src/components/slider.js
+++ src/components/slider.js
@@ -14,6 +14,15 @@ export default function SimpleSlider() {
     return (
       <Slider {...settings} className='slider-wrapper'>
         <div className='spotify-episodes'>
+            <iframe src="https://open.spotify.com/embed-podcast/episode/3bbYxUk7czx1nnywAUYkMt"
+                title="episode"
+                height="232" 
+                frameborder="0" 
+                allowtransparency="true" 
+                allow="encrypted-media">
+            </iframe>
+        </div> 
+        <div className='spotify-episodes'>
             <iframe src="https://open.spotify.com/embed-podcast/episode/0LPZayZYdzmwo6VZxzK4tV"
                 title="episode"
                 height="232" 
@@ -57,16 +66,7 @@ export default function SimpleSlider() {
                 allowtransparency="true" 
                 allow="encrypted-media">
             </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/4PNw6JKOwcTeLL1e7vhMtk"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>                                              
+        </div>                                             
       </Slider>
     );
   }
