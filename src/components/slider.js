7c3d651240e0e6098013920b2c3526e59508819d
kaenovsky <martinkaen@gmail.com>
2022-02-11 16:35:32 -0300

    ep array

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,72 +1,40 @@
 import React from "react"
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
 
 export default function SimpleSlider() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <Slider {...settings} className='slider-wrapper'>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/3bbYxUk7czx1nnywAUYkMt"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div> 
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/0LPZayZYdzmwo6VZxzK4tV"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/2M3HEnbLd2ffZyVaFp8XLY"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/7IY7t83GOAVtoeP7UsoRka"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/13L9HxIt0FX1jggUsv7W1f"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>
-        <div className='spotify-episodes'>
-            <iframe src="https://open.spotify.com/embed-podcast/episode/09TweJRu6oqk11PHIIILH7"
-                title="episode"
-                height="232" 
-                frameborder="0" 
-                allowtransparency="true" 
-                allow="encrypted-media">
-            </iframe>
-        </div>                                             
-      </Slider>
-    );
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   }
+  var episodesIds = [
+    "57uMLHgCu5TY9XgltKUggK",
+    "2XzsuktXELPmxCCF4tjniw",
+    "3bbYxUk7czx1nnywAUYkMt",
+    "0LPZayZYdzmwo6VZxzK4tV",
+    "2M3HEnbLd2ffZyVaFp8XLY",
+    "7IY7t83GOAVtoeP7UsoRka",
+  ];
+  return (
+    <Slider {...settings} className="slider-wrapper">
+      {episodesIds.map(episodeId => {
+        return (
+          <div className="spotify-episodes">
+            <iframe
+              src={`https://open.spotify.com/embed-podcast/episode/${episodeId}`}
+              title="episode"
+              height="232"
+              frameborder="0"
+              allowtransparency="true"
+              allow="encrypted-media"
+            ></iframe>
+          </div>
+        );
+      })}
+    </Slider>
+  )
+}
